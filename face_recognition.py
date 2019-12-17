fc9ac7ab28086f48c99f401582903c93c07b9530
aaronhsueh0506 <58929071+aaronhsueh0506@users.noreply.github.com>
2019-12-17 13:27:43 +0800

    Update and rename VVS_drone.py to face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -25,41 +25,6 @@ facerec = dlib.face_recognition_model_v1(face_rec_model_path)
 win = dlib.image_window()
 descriptors = []
 
-### take feature and save###
-
-# for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
-#     print("Processing file: {}".format(f))
-#     img = dlib.load_rgb_image(f)
-#
-#     win.clear_overlay()
-#     win.set_image(img)
-#     # 人臉辨識
-#     dets = detector(img, 0)
-#     print("Number of faces detected: {}".format(len(dets)))
-#     for k, d in enumerate(dets):
-#         print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-#             k, d.left(), d.top(), d.right(), d.bottom()))
-#         # 特徵
-#         shape = sp(img, d)
-#         print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-#                                                   shape.part(1)))
-#         # Draw the face landmarks on the screen.
-#         win.add_overlay(shape)
-#
-#         # 3.取得Vector，128維特徵向量
-#         face_descriptor = facerec.compute_face_descriptor(img, shape)
-#
-#         # 轉換numpy array格式
-#         v = numpy.array(face_descriptor)
-#         descriptors.append(v)
-#     win.add_overlay(dets)
-#     # dlib.hit_enter_to_continue()
-# numpy.save('data.npy',descriptors)
-
-# a=numpy.mean(numpy.load('aaron.npy'), axis=1)
-# b=numpy.mean(numpy.load('tommy.npy'), axis=1)
-# c=numpy.mean(numpy.load('tony.npy'), axis=1)
-
 ### read enroll data ###
 candidate = ['aaron', 'tommy', 'tony']
 for i in candidate:
